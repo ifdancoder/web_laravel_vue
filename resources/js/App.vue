b33b67cd84dae8aca38e0acc7764a141a6c53009
IfDANCodeR <ifdancoder@gmail.com>
2024-09-28 22:04:38 +0800

    Now Vue is working

---
+++ resources/js/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <nav>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+        </nav>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+}
+
+nav {
+    padding: 30px;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,8 @@
 import './bootstrap';
+import router from './routes.js';
+import App from './App.vue';
 import { createApp } from 'vue'
+
+console.log(router);
 
 createApp(App).use(router).mount('#app')
